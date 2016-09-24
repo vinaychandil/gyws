fe53fbc0d2e284fccc61b6076159ede749a9b6c9
AnandKumar22 <ak8574779@gmail.com>
2016-09-24 17:49:40 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -11,8 +11,9 @@
                                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                                 <li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
-                                <li><a href="#"><i class="fa fa-google+"></i></a></li>
-                                <li><a href="#"><i class="fa fa-quora"></i></a></li>
+                                <li><a href="#"><i class="fa fa-phone-square"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                               <!-- <li><a href="#"><i class="fa fa-wikipedia"></i></a></li>-->
                             </ul>
                             <div class="search">
                                 <form role="form">
@@ -25,7 +26,7 @@
                 </div>
             </div><!--/.container-->
         </div><!--/.top-bar-->
-
+           
         <nav class="navbar navbar-inverse" role="banner">
             <div class="container">
                 <div class="navbar-header">
@@ -42,7 +43,7 @@
                     <ul class="nav navbar-nav">
                         <li><a href="index.php">Home</a></li>
                         <li class="dropdown">
-                            <a href="about-us.php" class="dropdown-toggle" data-toggle="dropdown">About Us<i class="fa fa-angle-down"></i></a>
+                            <a href="about-us.php" class="dropdown-toggle" data-toggle="dropdown">About Us &nbsp;<i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Who we Are?</a></li>
                                 <li><a href="#">Vision</a></li>
@@ -53,46 +54,51 @@
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="services.php" class="dropdown-toggle" data-toggle="dropdown">Initiatives<i class="fa fa-angle-down"></i></a>
+                            <a href="services.php" class="dropdown-toggle" data-toggle="dropdown">Initiatives &nbsp;<i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Jagriti Vidya Mandir</a></li>
-                                <li><a href="#">KGP Blood Connect</a></li>
+                                <li><a href="#">KGP BloodConnect</a></li>
                                 <li><a href="#">Social Cell</a></li>
                                 <li><a href="#">Pedals For Progress</a></li>
                                 <li><a href="#">e-Samadhan</a></li>
-                                <li><a hrf="#">Prayas</a></li>
+                                <li><a hrf="#">Prayas</a></li><hr>
                                 <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">UDYAT<i class="fa fa-angle-down"></i></a>
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="text-decoration:underline;font-size:130%"><center>Udyat &nbsp;</center><!--<i class="fa fa-angle-down">--></i></a>
+                                    <div id="hidden">
                                     <ul class="dropdown-menu">
-                                        <li><a href="#">Gujrati Coaching</a></li>
+                                        <li><a href="#">Computer Coaching</a></li>
                                         <li><a href="#">Helping Hands</a></li>
-                                        <li><a href="#">Teacher's traning</a></li>
+                                        <li><a href="#">Teachers' Training</a></li>
                                     </ul>
+                                    </div>
                                 </li>
                              </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="portfolio.php" class="dropdown-toggle" data-toggle="dropdown">Members<i class="fa fa-angle-down"></i></a>
+                            <a href="portfolio.php" class="dropdown-toggle" data-toggle="dropdown">Members &nbsp;<i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">Advisory Committe</a></li>
-                                <li><a href="#">Governing Body<br> Members</a></li>
-                                <li><a href="#">Coordinators</a></li>
-                                <li><a href="#">Post-gratuate/<br>Research Scholers</a></li>
-                                <li><a href="#">Heads</a></li>
-                                <li><a href="#">Senior Executive<br> Members</a></li>
+                                <li><a href="#">Allumni Members</a></li>
+                                <li><a href="#">Governing Body<br> Members</a></li><hr>
+                                <li><a href="#" style="text-decoration:underline;font-size:130%"><center>Coordinators</center></a></li>
+                                <div id="hidden">
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Under Graduate</a></li>
+                                        <li><a href="#">Post Graduate</a></li>
+                                        <li><a href="#">Research Scholars</a></li>
+                                    </ul>
+                                </div>
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Support Us <i class="fa fa-angle-down"></i></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Support Us &nbsp;<i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
-                                <li><a href="blog-item.php">Blog Single</a></li>
-                                <li><a href="pricing.php">Pricing</a></li>
-                                <li><a href="404.php">404</a></li>
-                                <li><a href="shortcodes.php">Shortcodes</a></li>
+                                <li><a href="blog-item.php">Educate A Child</a></li>
+                                <li><a href="pricing.php">Earn A Smile</a></li>
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="blog.php" class="dropdown-toggle" data-toggle="dropdown">Donate <i class="fa fa-angle-down"></i></a>
+                            <a href="blog.php" class="dropdown-toggle" data-toggle="dropdown">To Donate &nbsp;<i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">General Donations</a></li>
                                 <li><a href="#">Educate A Child</a></li>
