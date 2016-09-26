3380ebbebbeb1f63ac5d5e7f290393d54201fff6
Vinay Chandil <vinaychandil17@gmail.com>
2016-09-26 22:15:09 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+ <footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2016 <a target="_blank" href="http://www.gyws.org/" title="An NGO run by the students of IIT KHARAGPUR">GOPALI YOUTH WELFARE SOCIETY</a>. All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google+"></i></a></li>
+                        <li><a href="#"><i class="fa fa-quora"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
